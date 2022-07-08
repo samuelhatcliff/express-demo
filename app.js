9ce929812081174c4cd4ae91723dd5ba1b774453
Samuel Hatcliff <samuel.hatcliff@gmail.com>
2022-07-08 16:02:41 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("HOMEPAGE")
+})
+app.get('/dogs', (req, res) => {
+    console.log(req)
+    console.log("you got dogs!")
+    res.send("WOOOOF")
+})
+
+app.listen(3000, () => {
+    console.log('App on port 3000')
+})
